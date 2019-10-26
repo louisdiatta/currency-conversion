057124d3fc06bb3c77d848d862705e65de4af93b
Luwee <ldiatta@kaylene-group.com>
2019-10-26 16:15:37 +0000

    Added feign client

--- src/main/java/com/luwee/currencyconversion/web/rest/CurrencyConversionController.java
+++ src/main/java/com/luwee/currencyconversion/web/rest/CurrencyConversionController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.luwee.currencyconversion.VM.CurrencyConversionBean;
+import com.luwee.currencyconversion.web.rest.clients.CurrencyExchangeServiceProxy;
 
 @RestController
 public class CurrencyConversionController {
 
   private Logger logger = LoggerFactory.getLogger(this.getClass());
+  
+  @Autowired
+  private CurrencyExchangeServiceProxy proxy;
 
   @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
   public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
@@ -32,6 +37,18 @@ public class CurrencyConversionController {
         uriVariables);
 
     CurrencyConversionBean response = responseEntity.getBody();
+
+    return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+        quantity.multiply(response.getConversionMultiple()), response.getPort());
+  }
+  
+  @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+  public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+      @PathVariable BigDecimal quantity) {
+
+    CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+    logger.info("{}", response);
 
     return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
         quantity.multiply(response.getConversionMultiple()), response.getPort());
---
+++ src/main/java/com/luwee/currencyconversion/web/rest/clients/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,15 @@
+package com.luwee.currencyconversion.web.rest.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.luwee.currencyconversion.VM.CurrencyConversionBean;
+
+@FeignClient(name="forex-service", url="localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+  @GetMapping("/currency-exchange/from/{from}/to/{to}")
+  public CurrencyConversionBean retrieveExchangeValue
+    (@PathVariable("from") String from, @PathVariable("to") String to);
+}
